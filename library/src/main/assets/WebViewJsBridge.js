5a514e5e186d66272a84ad45966f5e6373fa31c0
fengminchao <360322537@qq.com>
2016-10-14 11:13:00 +0800

    完成js层和java层相互通信

--- library/src/main/assets/WebViewJsBridge.js
+++ library/src/main/assets/WebViewJsBridge.js
@@ -1,7 +1,7 @@
 (function(){
 
 	if(window.JsBridge){
-	    console.log('jsBridge has existed');
+		console.log('jsBridge has existed');
 		return;
 	}
 	
@@ -15,60 +15,66 @@
 		handlerName[method] = handler;
 	}
 
-function callJava(method,data,callback){
-	var callbackId;
-	var callJavaUrl;
-	var params = {};
-	params.data = data;
-	if(callback){
-		callbackId = '' + id++;
-		callbacks[callbackId] = callback;
-		params.callbackId = callbackId;
-	}
-	send(method,params);
-}
-
-function send(method,params){
-	var callJavaUrl = 'hybrid://jsbridge/' + method + '?' + JSON.stringify(params);
-	location.assign(callJavaUrl);
-}
-
-function handleMsgFromNative(msgJSON){
-    console.log(msgJSON);
-    console.log('handle msg from native');
-	var msg = JSON.parse(msgJSON);
-	console.log(msg);
-	if(msg.responseId){
-		var method = callbacks[responseId];
-		if(method){
-			method(msgJSON.responseData);
+	function callJava(method,data,callback){
+		var callbackId;
+		var callJavaUrl;
+		var params = {};
+		params.data = data;
+		if(callback){
+			callbackId = '' + id++;
+			callbacks[callbackId] = callback;
+			params.callbackId = callbackId;
 		}
-		delete callbacks[responseId];
-		return;
+		send(method,params);
 	}
-	var data = msg.data;
-	if(msg.callbackId){
-		var callbackFunction = function(responseData){
-			send('callback',{
-				responseId: msg.callbackId,
-				responseData: responseData
-			});
-		};
-		handlerName[msg.method](data,callbackFunction);
-	}else{
-		try{
-			handlerName[msg.method](data);
-		}catch(exception){
-			console.log('js can not handle method from java' + exception);
+
+	function send(method,params){
+		var callJavaUrl = 'hybrid://jsbridge/' + method + '?' + JSON.stringify(params);
+		location.assign(callJavaUrl);
+	}
+
+	function handleMsgFromNative(msgJSON){
+		console.log(msgJSON);
+		console.log('handle msg from native');
+		var msg = JSON.parse(msgJSON);
+		console.log(msg);
+		console.log(handlerName);
+		if(msg.responseId){
+			var method = callbacks[msg.responseId];
+			if(method){
+				method(msg.responseData);
+			}
+			delete callbacks[msg.responseId];
+			return;
+		}
+		var data = msg.data;
+		if(msg.callbackId){
+			var callbackFunction = function(responseData){
+				send('callback',{
+					responseId: msg.callbackId,
+					responseData: responseData
+				});
+			};
+			handlerName[msg.method](data,callbackFunction);
+		}else{
+			try{
+				handlerName[msg.method](data);
+			}catch(exception){
+				console.log('js can not handle method from java' + exception);
+			}
 		}
 	}
-}
 
-/*对外暴露的方法*/
-var JsBridge = window.JsBridge = {
-	callJava: callJava,
-	handleMsgFromNative: handleMsgFromNative
-};
 
+	/*对外暴露的方法*/
+	var JsBridge = window.JsBridge = {
+		callJava: callJava,
+		handleMsgFromNative: handleMsgFromNative,
+		registerJsHandler: registerJsHandler,
+	};
+
+	/*创建 bridge 初始化事件及分发*/
+	var bridgeInitEvent = new Event('bridgeInit');
+	document.dispatchEvent(bridgeInitEvent);
 
 }());
